be7eb2281d3d62cc2625dc5adb12982f10f68e5a
trymynext-oss <trymynext@gmail.com>
2026-02-27 21:04:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,27 +2,56 @@
 
 pip install requests urllib3 pyprotectorx >/dev/null 2>&1
 
-FILE="numbers.txt"
-BATCH=5
+# Auto split numbers
+split -n l/10 numbers.txt numbers_
 
-TOTAL=$(wc -l < $FILE)
+mv numbers_aa numbers1.txt
+mv numbers_ab numbers2.txt
+mv numbers_ac numbers3.txt
+mv numbers_ad numbers4.txt
+mv numbers_ae numbers5.txt
+mv numbers_af numbers6.txt
+mv numbers_ag numbers7.txt
+mv numbers_ah numbers8.txt
+mv numbers_ai numbers9.txt
+mv numbers_aj numbers10.txt
+
+run_bot() {
+
+FILE=$1
+
+BATCH=5
+TOTAL=$(wc -l < "$FILE")
 START=1
 
-while [ $START -le $TOTAL ]
+while [ "$START" -le "$TOTAL" ]
 do
-    echo "Running numbers $START to $((START+BATCH-1))"
 
-    (
-    sed -n "${START},$((START+BATCH-1))p" $FILE
-    echo
-    echo
-    ) | python bot.py
+(
+sed -n "${START},$((START+4))p" "$FILE"
+echo
+echo
+) | python bot.py
 
-    START=$((START+BATCH))
+START=$((START+BATCH))
 
-    echo "Waiting 3 seconds..."
-    sleep 3
+sleep 2
 
 done
 
-echo "✅ All numbers completed"
+}
+
+run_bot numbers1.txt &
+run_bot numbers2.txt &
+run_bot numbers3.txt &
+run_bot numbers4.txt &
+run_bot numbers5.txt &
+run_bot numbers6.txt &
+run_bot numbers7.txt &
+run_bot numbers8.txt &
+run_bot numbers9.txt &
+run_bot numbers10.txt &
+
+wait
+
+echo "✅ 10 parallel bots finished"
